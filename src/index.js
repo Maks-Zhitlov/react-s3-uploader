e4de4834706f9a8f0802c3f1336397d2612b3748
Maks Zhitlov <maks.zhitlov@gmail.com>
2018-02-22 18:01:54 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example';
 import './style.css';
+import S3Uploader from "./s3ImageUploader/index";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Example/>, document.getElementById('root'));
+class ExampleApp extends Component {
+    render() {
+        return (
+            <div className="example-app">
+                <br/>
+                <h2>Example of usage <a href="#" target="_blank">s3Uploader</a></h2>
+                <br/>
+                <div className="app-container">
+                    <S3Uploader
+                        accept="image/*"
+                        signingUrl="/s3/sign"
+                        server="http://localhost:7500"
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<ExampleApp/>, document.getElementById('root'));
 registerServiceWorker();
--- src/s3ImageUploader/components/FileList.js
+++ src/s3ImageUploader/components/FileList.js
@@ -22,6 +22,7 @@ class FileList extends Component {
 
         const arr = files.map((el) => {
             const trClassName = trClassNames[el.status];
+            const isImage = el.type.includes('image');
             return (
                 <tr key={el.name} className={trClassName || ''}>
                     <td>{el.name}</td>
@@ -32,14 +33,18 @@ class FileList extends Component {
                         {el.status || 'Not uploaded'}
                     </td>
                     <td>
-                        <button type="button" title="View" onClick={onViewItem(el)}>
-                            <svg fill={iconColor} height={iconSize} viewBox="0 0 24 24" width={iconSize}
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0 0h24v24H0z" fill="none"/>
-                                <path
-                                    d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
-                            </svg>
-                        </button>
+                        {
+                            isImage ?
+                                <button type="button" title="View" onClick={onViewItem(el)}>
+                                    <svg fill={iconColor} height={iconSize} viewBox="0 0 24 24" width={iconSize}
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M0 0h24v24H0z" fill="none"/>
+                                        <path
+                                            d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+                                    </svg>
+                                </button> : []
+                        }
+
                         <button disabled={disableUpload} type="button" title="Remove" onClick={onDeleteClick(el)}>
                             <svg fill={iconColor} height={iconSize} viewBox="0 0 24 24" width={iconSize}
                                  xmlns="http://www.w3.org/2000/svg">
